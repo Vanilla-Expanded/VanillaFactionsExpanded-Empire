d7eb119ca8d28a4b3e2b4f0cad7a2a67320976cc
ANRimworld <allie.noble.an@gmail.com>
2022-09-13 11:05:51 -0700

    Bestow Title Ritual and Abilities

---
+++ 1.3/Source/VFEEmpire/VFEEmpire/Comps/CompProperties_AbilityStartBestower.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VFEEmpire;
+
+public class CompProperties_AbilityStartBestower : CompProperties_AbilityStartRitualOnPawn
+{
+    public CompProperties_AbilityStartBestower()
+    {
+        this.compClass = typeof(CompAbilityEffect_StartBestowing);
+    }
+    public RoyalTitleDef titleDef;
+}
