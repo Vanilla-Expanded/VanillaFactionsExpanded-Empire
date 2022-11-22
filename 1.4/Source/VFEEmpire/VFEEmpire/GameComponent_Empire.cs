5c9268f4f890152b18f9a2b455abbdec5a36f94e
ANRimworld <allie.noble.an@gmail.com>
2022-11-22 10:27:47 -0800

    Deserter Goodwill logic, Deserter Hideout work

--- 1.4/Source/VFEEmpire/VFEEmpire/GameComponent_Empire.cs
+++ 1.4/Source/VFEEmpire/VFEEmpire/GameComponent_Empire.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Verse;
+using RimWorld;
 
 namespace VFEEmpire;
 
@@ -11,11 +13,55 @@ public class GameComponent_Empire : GameComponent
     public Dictionary<Pawn, int> LastRoyalGossipTick = new();
     
     private List<int> valuesList;
-
+    
     public Dictionary<ThingWithComps, Pawn> artCreator = new();//Nope doing tagged string compares was too disgusting
     private List<ThingWithComps> tmpThings = new();
     private List<Pawn> tmpPawn = new();
     public GameComponent_Empire(Game game) => Instance = this;
+    private int checkTick;
+    private Faction deserter;
+
+
+    //For tracking if deserters should be hostile
+    public override void GameComponentTick()
+    {
+        base.GameComponentTick();
+        if (Find.TickManager.TicksGame > checkTick && Deserter != null)
+        {
+            checkTick = Find.TickManager.TicksGame + 6000;//0 reason to check it often
+            bool hostileEmpire = Faction.OfEmpire.HostileTo(Faction.OfPlayer);
+            bool hostileDeserter = Deserter.HostileTo(Faction.OfPlayer);
+            bool hasTitle = false;
+            foreach (var map in Find.Maps.Where(x => x.ParentFaction == Faction.OfPlayer))
+            {
+                if (map.mapPawns.FreeColonists.Any(x => x.royalty?.HasAnyTitleIn(Faction.OfEmpire) == true))
+                {
+                    hasTitle = true;
+                    break;
+                }
+            }
+            if (hostileEmpire && hostileDeserter || (hostileDeserter && !hasTitle))
+            {
+                Faction.OfPlayer.SetRelationDirect(Deserter, FactionRelationKind.Neutral,false);
+            }
+            if(!hostileEmpire && !hostileDeserter && hasTitle)
+            {
+                Faction.OfPlayer.SetRelationDirect(Deserter, FactionRelationKind.Hostile, false);
+            }
+        }
+    }
+
+    public Faction Deserter
+    {
+        get
+        {
+            if (deserter == null)
+            {
+                deserter = Find.FactionManager.FirstFactionOfDef(InternalDefOf.VFEE_Deserters);
+            }
+            return deserter;
+        }
+    }
 
     public override void ExposeData()
     {
---
+++ 1.4/Source/VFEEmpire/VFEEmpire/QuestNode/QuestNode_DeserterGoodWill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using RimWorld.QuestGen;
+
+namespace VFEEmpire
+{
+    public class QuestNode_DeserterGoodWill : QuestNode
+    {
+        protected override void RunInt()
+        {
+            var quest = QuestGen.quest;
+            var slate = QuestGen.slate;
+            //not using it in test run as I dont want to prevent quest from starting
+            if (GameComponent_Empire.Instance.Deserter == null) { return; }
+            var part_DamageGoodWill = new QuestPart_FactionRelationChange()
+            {
+                inSignal = QuestGenUtility.HardcodedSignalWithQuestID(QuestGen.slate.Get<string>("inSignal")),
+                faction = GameComponent_Empire.Instance.Deserter,
+                canSendHostilityLetter = false,
+                relationKind = makeHostile.GetValue(slate) ? FactionRelationKind.Hostile : FactionRelationKind.Ally,
+            };
+            quest.AddPart(part_DamageGoodWill);
+        }
+        protected override bool TestRunInt(Slate slate)
+        {
+            return true;
+        }
+        public SlateRef<bool> makeHostile;
+    }
+}
