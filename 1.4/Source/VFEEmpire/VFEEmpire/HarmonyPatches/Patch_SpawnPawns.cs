0f761dec35a926f84b6225b1a999c6270a7ceb23
Joshua <legodude17@users.noreply.github.com>
2023-03-09 15:08:17 -0800

    Fix NRE in Patch_SpawnPawns

--- 1.4/Source/VFEEmpire/VFEEmpire/HarmonyPatches/Patch_SpawnPawns.cs
+++ 1.4/Source/VFEEmpire/VFEEmpire/HarmonyPatches/Patch_SpawnPawns.cs
@@ -19,28 +19,26 @@ public static class Patch_SpawnPawns
     [HarmonyPostfix]
     public static void Postfix(IncidentWorker_NeutralGroup __instance, IncidentParms parms, ref List<Pawn> __result)
     {
-        if (pawnGroupKindDef(__instance) == PawnGroupKindDefOf.Trader && parms.faction.def.techLevel >= TechLevel.Industrial && parms.target is Map map
+        if (pawnGroupKindDef(__instance) == PawnGroupKindDefOf.Trader && parms?.faction?.def != null && parms.faction.def.techLevel >= TechLevel.Industrial
+         && parms.target is Map map
          && parms.faction.def.BaselinerChance > 0)
         {
-            var noble = map.mapPawns.FreeColonists.OrderByDescending(p => p.royalty?.GetCurrentTitle(Faction.OfEmpire)?.seniority ?? 0).FirstOrDefault();
-            if (noble != null)
+            var noble = map.mapPawns.FreeColonists.OrderByDescending(p => p?.royalty?.GetCurrentTitle(Faction.OfEmpire)?.seniority ?? 0).FirstOrDefault();
+            var title = noble?.royalty.GetCurrentTitle(Faction.OfEmpire);
+            if (title?.seniority > RoyalTitleDefOf.Knight.seniority)
             {
-                var title = noble.royalty.GetCurrentTitle(Faction.OfEmpire);
-                if (title.seniority > RoyalTitleDefOf.Knight.seniority)
+                var progress = Mathf.InverseLerp(RoyalTitleDefOf.Knight.seniority, VFEE_DefOf.Stellarch.seniority, title.seniority);
+                var chance = Mathf.Lerp(0.01f, 1f, progress) * VFEEmpireMod.Settings.deserterChanceMult;
+                if (Rand.Chance(chance))
                 {
-                    var progress = Mathf.InverseLerp(RoyalTitleDefOf.Knight.seniority, VFEE_DefOf.Stellarch.seniority, title.seniority);
-                    var chance = Mathf.Lerp(0.01f, 1f, progress) * VFEEmpireMod.Settings.deserterChanceMult;
-                    if (Rand.Chance(chance))
+                    var num = Rand.Range(1, 4);
+                    for (var i = 0; i < num; i++)
                     {
-                        var num = Rand.Range(1, 4);
-                        for (var i = 0; i < num; i++)
-                        {
-                            var pawn = PawnGenerator.GeneratePawn(VFEE_DefOf.VFEE_Deserter, parms.faction);
-                            var loc = CellFinder.RandomClosewalkCellNear(parms.spawnCenter, map, 5);
-                            GenSpawn.Spawn(pawn, loc, map);
-                            parms.storeGeneratedNeutralPawns?.Add(pawn);
-                            __result.Add(pawn);
-                        }
+                        var pawn = PawnGenerator.GeneratePawn(VFEE_DefOf.VFEE_Deserter, parms.faction);
+                        var loc = CellFinder.RandomClosewalkCellNear(parms.spawnCenter, map, 5);
+                        GenSpawn.Spawn(pawn, loc, map);
+                        parms.storeGeneratedNeutralPawns?.Add(pawn);
+                        __result.Add(pawn);
                     }
                 }
             }
