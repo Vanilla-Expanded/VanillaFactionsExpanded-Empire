6f0a53d9578dac38bdf3db245133e96ec12de086
Joshua <legodude17@users.noreply.github.com>
2023-03-29 16:03:25 -0700

    Techfriar crown now works with RR

--- 1.4/Source/VFEEmpire/VFEEmpire/Comps/CompResearchRefresh.cs
+++ 1.4/Source/VFEEmpire/VFEEmpire/Comps/CompResearchRefresh.cs
@@ -1,14 +1,24 @@
-﻿using RimWorld;
+﻿using System.Collections.Generic;
+using RimWorld;
 using Verse;
 
 namespace VFEEmpire;
 
+[StaticConstructorOnStartup]
 public class CompResearchRefresh : ThingComp
 {
+    private static readonly HashSet<string> researchJobs = new()
+    {
+        "Research",
+        "RR_Research"
+    };
+
+
     public override void CompTick()
     {
         base.CompTick();
-        if (parent.holdingOwner.Owner is Pawn_ApparelTracker { pawn: { jobs.curDriver: JobDriver_Research, needs: { joy: var joy, rest: var rest } } })
+        if (parent.holdingOwner.Owner is Pawn_ApparelTracker { pawn: { CurJobDef: { defName: var job }, needs: { joy: var joy, rest: var rest } } }
+         && researchJobs.Contains(job))
         {
             joy?.tolerances?.Notify_JoyGained(-1f / 0.65f, VFEE_DefOf.VFEE_Research);
             joy?.GainJoy(0.36f / 2500f, VFEE_DefOf.VFEE_Research);
