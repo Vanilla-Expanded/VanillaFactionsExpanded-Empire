a0659ff16c19b88373220fa0eb9918ede632b1f7
SokyranTheDragon <exploder1531@gmail.com>
2025-03-10 00:05:15 +0100

    Prevent royal pawns from being discarded

---
+++ 1.5/Source/VFEEmpire/VFEEmpire/HarmonyPatches/Patch_WorldPawns_PassToWorld.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld.Planet;
+using Verse;
+
+namespace VFEEmpire.HarmonyPatches;
+
+[HarmonyPatch(typeof(WorldPawns), nameof(WorldPawns.PassToWorld))]
+public static class Patch_WorldPawns_PassToWorld
+{
+    static void Prefix(Pawn pawn, ref PawnDiscardDecideMode discardMode)
+    {
+        // A pawn in the hierarchy can be discarded by the game if they were
+        // spawned at least once. This patch should ensure this doesn't happen.
+        if (discardMode != PawnDiscardDecideMode.KeepForever &&
+            !pawn.Discarded &&
+            !pawn.Dead &&
+            !pawn.IsColonist &&
+            WorldComponent_Hierarchy.Instance.TitleHolders.Contains(pawn))
+            discardMode = PawnDiscardDecideMode.KeepForever;
+    }
+}
